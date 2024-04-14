926825087dcbcad60b8e824a3cbc52fa0890fa6d
husanibragimov <ibragmovhusan283@gmail.com>
2024-04-14 11:37:28 +0500

    First Commit

---
+++ apps/users/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class AccountManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if username is None:
+            raise TypeError("Username didn't come")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, password=None, **extra_fields):
+        if not password:
+            raise TypeError("Password did not come!")
+        user = self.create_user(username, password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+GENDER = (
+    (0, "Male"),
+    (1, "Famale"),
+)
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=225, unique=True, db_index=True)
+    full_name = models.CharField(max_length=225, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True, unique=True)
+    date_birth = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True, db_index=True, null=True, blank=True)
+    gender = models.IntegerField(choices=GENDER, default=0)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    objects = AccountManager()
+
+    EMAIL_FIELD = 'username'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+
+    def __str__(self):
+        if self.full_name:
+            return f'{self.full_name}'
+        return f'{self.username}'
+
+
